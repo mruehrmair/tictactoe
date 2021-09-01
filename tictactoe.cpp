2c615828f5e0f31f0a8c117dc9d1a2f51bddf95c
Shiftingbytes <shiftingbytes24@gmail.com>
2021-09-01 23:36:02 +0200

    [TicTacToe] added game field

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,5 +1,31 @@
 #include "raylib.h"
 
+struct Field
+{
+    int width;
+    int height;
+    int x;
+    int y;
+    Color color;
+};
+
+void DrawMatrix(Field *matrix, int size)
+{
+    int x{0};
+    int y{0};
+    for (int i = 0; i < size; i++)
+    {
+        DrawRectangle(matrix[i].x + x, matrix[i].y + y, matrix[i].width, matrix[i].height, matrix[i].color);
+        x += matrix[i].width + 2;
+        bool newLine = !((i + 1) % 3);
+        if (newLine)
+        {
+            y += matrix[i].height + 2;
+            x = 0;
+        }
+    }
+}
+
 int main()
 {
     //window dimensions
@@ -7,19 +33,31 @@ int main()
     int height{600};
     // int windowStart{0};
 
+    //init fields
+    const int matrixSize{9};
+    Field matrix[matrixSize];
+    for (int i = 0; i < matrixSize; i++)
+    {
+        matrix[i].width = 200;
+        matrix[i].height = 200;
+        matrix[i].x = 0;
+        matrix[i].y = 0;
+        matrix[i].color = RED;
+    }
     //settings
     int targetFPS{60};
     // int speed{10};
     // int direction{10};
     // int fontSize{20};
     SetTargetFPS(targetFPS);
-    InitWindow(width, height, "Markus' Window!");
+    InitWindow(width, height, "TicTacToe");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+        //begin game logic
+        DrawMatrix(matrix, matrixSize);
         EndDrawing();
     }
-    
 }
